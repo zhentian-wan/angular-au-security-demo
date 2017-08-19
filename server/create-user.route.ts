0607078899e3c3fd872772eed8f38836c3611d51
Zhentian Wan <answer881215@gmail.com>
2017-08-19 14:28:33 +0300

    create sessionId with async await

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -2,8 +2,9 @@ import {Request, Response} from 'express';
 import {db} from './database';
 import {USERS} from './database-data';
 
-import * as argon from 'argon2';
+import * as argon2 from 'argon2';
 import {validatePassword} from './password-validation';
+import {randomBytes} from './security.utils';
 
 export function createUser (req: Request, res: Response) {
 
@@ -16,13 +17,14 @@ export function createUser (req: Request, res: Response) {
       errors
     });
   } else {
-    argon.hash(credentials.password)
-      .then(passwordDigest => {
-
-        const user = db.createUser(credentials.email, passwordDigest);
-
-        console.log(USERS);
-        res.status(200).json({id: user.id, email: user.email});
-      });
+    createUserAndSession(res, credentials);
   }
-};
+}
+
+async function createUserAndSession(res, credentials) {
+  const passwordDigest = await argon2.hash(credentials.password);
+  const user = db.createUser(credentials.email, passwordDigest);
+  const sessionId = await randomBytes(32).then(bytes => bytes.toString('hex'));
+  console.log(sessionId);
+  res.status(200).json({id: user.id, email: user.email});
+}
--- server/security.utils.ts
+++ server/security.utils.ts
@@ -2,19 +2,9 @@ const util = require('util');
 const crypto = require('crypto');
 
 // convert a callback based code to promise based
-const randomBytes = util.promisify(
+export const randomBytes = util.promisify(
   crypto.randomBytes
 );
 
-/* Example of callback based code
-crypto.randomBytes(32, (err, num) => {
-  console.log(num);
-});
-*/
-
-// Since randomBytes function now is promise based
-randomBytes(32)
-  .then(num => console.log(num))
-  .catch(err => console.error(err));
 
 
