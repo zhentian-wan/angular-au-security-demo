5da904fe59dd15c8e096811bda225d6aa72dcbd6
Zhentian Wan <answer881215@gmail.com>
2017-09-04 16:24:56 +0300

    get user data on page refresh

--- server/get-user.route.ts
+++ server/get-user.route.ts
@@ -2,11 +2,15 @@ import {Request, Response} from 'express';
 import {sessionStore} from './session-store';
 
 export function getUser(req: Request, res: Response) {
+  // Get sessionid from cookies
   const sessionId = req.cookies['SESSIONID'];
+  // get user according to the session id from the session storage
   const user = sessionStore.findUserBySessionId(sessionId);
   if (user) {
+    // if there is user, send successful response
     res.status(200).json(user);
   } else {
+    // if there is no user, send empty response
     res.sendStatus(204);
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,7 @@ import {User} from '../model/user';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/shareReplay';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/do';
 
 export const ANONYMOUS_USER: User = {
@@ -15,13 +16,18 @@ export const ANONYMOUS_USER: User = {
 @Injectable()
 export class AuthService {
 
-  subject = new BehaviorSubject<User>(ANONYMOUS_USER);
-  user$: Observable<User> = this.subject.asObservable();
+  subject = new BehaviorSubject<User>(undefined);
+  // filter out undefined user
+  user$: Observable<User> = this.subject.asObservable().filter(user => !!user);
   isLoggedIn$: Observable<boolean> = this.user$.map(user => !!user.id);
   isLoggedOut$: Observable<boolean> = this.isLoggedIn$.map(isLoggedIn => !isLoggedIn);
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.http.get<User>('/api/user')
+      // when there is valid session id, emit the user$
+      .subscribe((user) => this.subject.next(user ? user : ANONYMOUS_USER));
+  }
 
   signUp(email: string, password: string) {
     return this.http.post<User>('/api/signup', {
