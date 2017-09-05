91c59c21356c58f60553da7ba9e55aabe78af27d
Zhentian Wan <answer881215@gmail.com>
2017-09-05 14:10:59 +0300

    merge with master

--- ts-node-68ee3ed04ed2470b254e92d708a85007aa11ce18f7f92036bd1ad817d7932003/c9387a2cdb4e320a1224b55969fc80a485fe4d8795f979579de2d9007faa6b88/cdf0b09400846b8e37b0507b1dadad7bdb0811dbf0785f82385f6e5457feb0f0.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var util = require('util');
-var crypto = require('crypto');
-// convert a callback based code to promise based
-exports.randomBytes = util.promisify(crypto.randomBytes);
-//# sourceMappingURL=/Users/zhentianwan/Documents/Programming/Angular2/angular-security-course/ts-node-68ee3ed04ed2470b254e92d708a85007aa11ce18f7f92036bd1ad817d7932003/c9387a2cdb4e320a1224b55969fc80a485fe4d8795f979579de2d9007faa6b88/cdf0b09400846b8e37b0507b1dadad7bdb0811dbf0785f82385f6e5457feb0f0.js.map
