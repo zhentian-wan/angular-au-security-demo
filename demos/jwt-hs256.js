0dc7a16ff75e323ffe2b1ebcebbaac52dbdcc8a2
Zhentian Wan <answer881215@gmail.com>
2017-09-15 11:12:05 +0300

    simple demo for jwt

---
+++ demos/jwt-hs256.js
@@ -0,0 +1,17 @@
+var jwt = require('jsonwebtoken');
+
+
+
+var secretKey = 'secret-key';
+
+var payload = {
+  name: 'Alice'
+};
+
+
+// create a JWT
+var newToken = jwt.sign(payload, secretKey, {
+  algorithm: 'HS256'
+});
+
+console.log("JWT created:", newToken);
